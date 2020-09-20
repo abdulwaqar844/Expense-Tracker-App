b2d39e68aa3a6568ce9567a236e7067b183ed9d5
abdulwaqar844 <abdulwaqar844@users.noreply.github.com>
2020-09-20 22:58:34 +0500

    build folder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-        Welcome To Expese Tracker App Using React App
-        </h1>
-      </header>
-    </div>
 
-  );
+<h2>This Parent Compnents</h2>
+
+    );
 }
 
 export default App;
---
+++ src/Child.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function Child() {
+  return (
+    <h1>
+        This is child 
+    </h1>
+
+    );
+}
+
+export default Child;
