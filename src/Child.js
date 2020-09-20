8d9e64d45e13b936239ae0d53295a17f8b9e6cb0
abdulwaqar844 <abdulwaqar844@users.noreply.github.com>
2020-09-20 23:44:40 +0500

    Css Applied

--- src/Child.js
+++ src/Child.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './App.css';
+import './index.css';
 
 function Child() {
     return (
         <div className="container">
             <h1 className="header">
                 Expense Tracker App
-
-    </h1>
-
+            </h1>
+            <div className="top-container">
+            <h2>Income</h2>
+            <h2>$500</h2>
+            <h2>Expense</h2>
+            <h2>$500</h2>
+            </div>
         </div>
     );
 }
